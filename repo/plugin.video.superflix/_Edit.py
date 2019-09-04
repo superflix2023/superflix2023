d1ec72e46e208c35ca70e880e6e48df5553afbcb
superflix19 <53096561+superflix19@users.noreply.github.com>
2019-09-03 22:36:25 -0300

    oi

--- repo/plugin.video.superflix/_Edit.py
+++
@@ -1,4 +0,0 @@
-import xbmcaddon
-
-MainBase = 'aHR0cHM6Ly9naXRsYWIuY29tL3N1cGVyZmxpeDIwMTkvc3VwZXJmbGl4L3Jhdy9tYXN0ZXIvQWRkb24lMjBzdXBlcmZsaXglMjBCYXNl'.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.superflix')
